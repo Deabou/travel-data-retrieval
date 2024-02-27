fc09f3b0d75e381364cea78614c3b1c37ad35c96
Deabou <bourgeo282@gmail.com>
2024-02-26 20:35:30 -0600

    updated wikipedia scrapper class

--- aita_code_grab/wikipedia_scrap.py
+++ aita_code_grab/wikipedia_scrap.py
@@ -4,41 +4,40 @@ import json
 
 
 class WikipediaAirportScraper:
-    def __init__(self):
-        self.base_url = "https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_{letter}"
+    BASE_URL = (
+        "https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_{letter}"
+    )
 
-    def scrape_data_from_wikipedia(self, url):
+    @staticmethod
+    def _scrape_table_data(table):
+        table_data = []
+        rows = table.find_all("tr")
+        for row in rows:
+            columns = row.find_all("td")
+            row_data = [column.text.strip() for column in columns]
+            if row_data:
+                table_data.append(row_data)
+        return table_data
+
+    @classmethod
+    def scrape_data_from_wikipedia(cls, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         tables = soup.find_all("table")
         if tables:
-            first_table = tables[0]
-            table_data = []
-            rows = first_table.find_all("tr")
-            for row in rows:
-                columns = row.find_all("td")
-                row_data = []
-                for column in columns:
-                    row_data.append(column.text.strip())
-                if row_data:
-                    table_data.append(row_data)
-            return table_data
+            return cls._scrape_table_data(tables[0])
         else:
             return None
 
-    def scrape_and_convert_to_json(self):
+    @classmethod
+    def scrape_and_convert_to_json(cls):
         scraped_data = {}
-        for letter in range(65, 91):  # ASCII values for A to Z
-            url = self.base_url.format(letter=chr(letter))
+        for letter in range(65, 90):  # ASCII values for A to Z
+            url = cls.BASE_URL.format(letter=chr(letter))
             print(f"Scraping data from {url}...")
-            data = self.scrape_data_from_wikipedia(url)
+            data = cls.scrape_data_from_wikipedia(url)
             if data:
                 scraped_data[chr(letter)] = data
             else:
                 print(f"No tables found on the page for letter {chr(letter)}.")
         return json.dumps(scraped_data)
-
-
-scraper = WikipediaAirportScraper()
-json_data = scraper.scrape_and_convert_to_json()
-print(json_data)
